6ba94115ae60505518c95013e58a14205fb9a208
yamiGmer <148083837+yamiGmer@users.noreply.github.com>
2026-08-14 06:53:56 +0800

    Made second version of project section to have tall vertical tiles instead of grid patterns in medium screen and above. Implemented second version to App.jsx.

--- src/sections/ProjectsV2.jsx
+++ src/sections/ProjectsV2.jsx
@@ -40,93 +40,123 @@ const projects = [
     github: "#"
 }
 ];
-
-
+// 4 in a row, when 1 is hovered the others shrink and the chosen gets wider
 export const Projects = () => {
     return (
         <section id="projects" className="py-32 relative overflow-hidden">
             {/* BG GLOWS */}
             <div className="absolute top-1/4 right-0 w-96 h-96 bg-primary/5 rounded-full blur-3xl" />
             <div className="absolute bottom-1/4 left-0 w-64 h-64 bg-highlight/5 rounded-full blur-3xl" />
+
             <div className="container mx-auto px-6 relative z-10">
+
                 {/* Section Header */}
-                <div className="text-center mx-auto maxw-3xl mb-16">
-                    <span className="text-secondary-foreground text-sm font-medium tracking-wider uppercase animate-fade-in">Featured Works</span>
+                <div className="text-center mx-auto max-w-3xl mb-16">
+                    <span className="text-secondary-foreground text-sm font-medium tracking-wider uppercase animate-fade-in">
+                        Featured Works
+                    </span>
+
                     <h2 className="text-4xl md:text-5xl font-bold mt-4 mb-6 animate-fade-in animation-delay-100 text-secondary-foreground">
-                        Projects that {" "}
+                        Projects that{" "}
                         <span className="font-serif italic font-normal text-white">
                             make an impact.
                         </span>
                     </h2>
+
                     <p className="text-muted-foreground animate-fade-in animation-delay-200">
-                        A selection of my recent work, from complex web applications to innovative tools that solve real-world problems.
+                        A selection of my recent work, from complex web applications
+                        to innovative tools that solve real-world problems.
                     </p>
                 </div>
 
-                {/* Projects Grid */}
-
-                <div className="grid md:grid-cols-2 gap-8">
+                {/* Projects */}
+                <div className="grid grid-cols-1 md:flex gap-6 md:h-[600px]">
                     {projects.map((project, idx) => (
-                        <div 
-                        key={idx} 
-                        className="group glass rounded-2xl overflow-hiden animate-fade-in md:row-span-1"
-                        style={{animationDelay: `${(idx +1) * 100}ms`}}
+                        <div
+                            key={idx}
+                            className="group glass rounded-2xl overflow-hidden relative h-[500px] md:h-full flex-1 md:hover:flex-[2] transition-all duration-700 ease-in-out animate-fade-in"
+                            style={{animationDelay: `${(idx + 1) * 100}ms`}}
                         >
                             {/* Image */}
-                            <div className="relative overflow-hidden aspect-video">
-                                <img 
-                                src={project.image} 
-                                alt={project.title} 
-                                className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-110"
-                                /> 
-                                <div 
-                                className="absolute inset-0 bg-gradient-to-t from-card via-vard/50 to-transparent opacity-60"
-                                />
+                            <img
+                                src={project.image}
+                                alt={project.title}
+                                className="
+                                    absolute inset-0
+                                    w-full h-full
+                                    object-cover
+                                    transition-transform duration-700
+                                    group-hover:scale-110
+                                "
+                            />
 
-                                {/* Overlay Links */}
-
-                                {/* <div className="absolute inset-0 flex items-center justify-center gap-4 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                                    <a href={project.link} className="p-3 rounded-full glass hover:bg-primary hover:text-primary-foreground transition-all">
-                                        <ArrowUpRight className="h-5 w-5"/>
-                                    </a>
-                                    <a href={project.github} className="p-3 rounded-full glass hover:bg-primary hover:text-primary-foreground transition-all">
-                                        <BsGithub className="h-5 w-5"/>
-                                    </a>
-                                </div> */}
-                            </div>
+                            {/* Overlay */}
+                            <div className="
+                                absolute inset-0
+                                bg-gradient-to-t
+                                from-card via-card/50 to-transparent
+                                opacity-80
+                            " />
 
                             {/* Content */}
-                            <div className="p-6 space-y-4">
-                                <div className="flex items-start justify-between">
-                                    <h3 className="text-xl font-semibold group-hover:text-primary transition-colors">{project.title}</h3>
-                                    {/* <ArrowUpRight className="w-5 h-5 text-muted-foreground group-hover:text-primary group-hover:translate-x-1 group-hover:translate-y-1 transition-all"/> */}
-                                </div>
-                                <p className="text-muted-foreground text-sm">{project.description}</p>
-                                <div className="flex flex-wrap gap-2">
-                                    {project.tags.map((tag,tagIdx) => (
-                                    <span 
-                                    className="px-4 py-1.5 rounded-full bg-surface text-xs font-medium border border-border/50 text-muted-foreground hover:border-primary/50  hover:text-primary transition-all duration-300"
-                                    key={tagIdx}
-                                    >
-                                        {tag}
-                                    </span>
+                            <div className="absolute bottom-0 left-0 w-full p-6">
 
+                                {/* Title */}
+                                <div className="flex items-start justify-between mb-4">
+                                    <h3 className="
+                                        text-xl md:text-2xl
+                                        font-semibold
+                                        group-hover:text-primary
+                                        transition-colors
+                                    ">
+                                        {project.title}
+                                    </h3>
+                                </div>
+
+                                {/* Description - hidden until hover */}
+                                <div className="
+                                    overflow-hidden
+                                    max-h-0
+                                    opacity-0
+                                    translate-y-4
+                                    group-hover:max-h-32
+                                    group-hover:opacity-100
+                                    group-hover:translate-y-0
+                                    transition-all duration-500 ease-in-out
+                                ">
+                                    <p className="text-muted-foreground text-sm mb-4">
+                                        {project.description}
+                                    </p>
+                                </div>
+
+                                {/* Tags - always visible */}
+                                <div className="flex flex-wrap gap-2">
+                                    {project.tags.map((tag, tagIdx) => (
+                                        <span
+                                            key={tagIdx}
+                                            className="
+                                                px-4 py-1.5
+                                                rounded-full
+                                                bg-surface
+                                                text-xs font-medium
+                                                border border-border/50
+                                                text-muted-foreground
+                                                hover:border-primary/50
+                                                hover:text-primary
+                                                transition-all duration-300
+                                            "
+                                        >
+                                            {tag}
+                                        </span>
                                     ))}
                                 </div>
+
                             </div>
                         </div>
                     ))}
-
                 </div>
 
-                {/* View All CTA */}
-                {/* <div className="text-center mt-12 animate-fade-in animation-delay-500">
-                    <AnimatedBorderButton>
-                        View All Projects
-                        <ArrowUpRight className="w-5 h-5" />
-                    </AnimatedBorderButton>
-                </div> */}
             </div>
         </section>
-)
-}
+    );
+};
